c781386e7095fe2f9c60bbec61e467fc4837d707
Maxime-Villermin <38423993+Maxime-Villermin@users.noreply.github.com>
2021-03-03 11:34:15 +0100

    Add files via upload

---
+++ ProjetASPGestionProduits/ProjetASPGestionProduits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetASPGestionProduits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
