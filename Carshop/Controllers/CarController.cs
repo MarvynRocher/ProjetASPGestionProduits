d017c8c3d24d598f30dcbbdb53be849668eed63a
Maxime Villermin <maxime.villermin@vinci-energies.net>
2021-03-04 10:48:35 +0100

    Modif carController

--- Carshop/Controllers/CarController.cs
+++ Carshop/Controllers/CarController.cs
@@ -10,27 +10,21 @@ namespace Carshop.Controllers
     [Route("/voitures")]
     public class CarController : Controller
     {
+        
         private readonly ICarRepository _carRepository;
-    //    private readonly ICategoryRepository _categoryRepository;
-
 
         public CarController(ICarRepository carRepository)
         {
-          
             _carRepository = carRepository;
-           // _categoryRepository = categoryRepository;
         }
 
-      //  [HttpGet("{category?}")]
-      [HttpGet("/test")]
         public async Task<IActionResult> List()
         {
-           // var selectedCategory = !string.IsNullOrWhiteSpace(category) ? category : null;
+
             var carsListViewModel = new CarsListViewModel
             {
                 Voitures = await _carRepository.GetCars()
-                //(selectedCategory),
-                //CurrentCategory = selectedCategory ?? "All Cars"
+ 
             };
             return View(carsListViewModel);
         }
